069b5962a16933906ff277fa0e640d36c37704d3
MiguelOliveir0 <167824293+MiguelOliveir0@users.noreply.github.com>
2024-06-24 19:49:13 -0300

    Add files via upload

---
+++ miguel     oliveira.py
@@ -0,0 +1,8 @@
+print("Programa da repetição até digitar")
+
+user = int(input("Digite um número Inteiro"))
+while user != 0:
+    print("Diferente de zero!!!!")
+    user = int(input("Digite um um número Inteiro"))
+print("Fim do programa!!")
+    
